696e61b2bcc05f124200f9c2cbf9216c1629863d
박상준 <parksangjun@bagsangjun-ui-MacBookAir.local>
2024-03-23 15:31:09 +0900

    검색api추가

--- src/components/Api.js
+++ src/components/Api.js
@@ -14,3 +14,20 @@ export async function getList({ language = "en", page }) {
   const body = await response.json();
   return body;
 }
+
+export async function searchList({ keyword }) {
+  const query = `query=${keyword}`;
+  const response = await fetch(
+    `https://api.themoviedb.org/3/search/movie?${query}&api_key=23346acd7f707faa560f0cb43b7ceb43`,
+    {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzM0NmFjZDdmNzA3ZmFhNTYwZjBjYjQzYjdjZWI0MyIsInN1YiI6IjY1ZjkyMDhmNGI5YmFlMDE4MzdlN2E0MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Ssop6XkD3mGVgWcFSPvDwPl8Y2UMV6bIf3fzjkVjSLs",
+      },
+    }
+  );
+  const body = await response.json();
+  return body;
+}
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./nav.css";
+import { searchList } from "./Api.js";
 
 function Nav() {
+  const [keyword, setKeyword] = useState();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setKeyword(e.target.search.value);
+    searchList({ keyword });
+  };
   return (
     <div className="navBar">
       <div className="logo">MoviePark</div>
       <div className="menuModal">menu</div>
       <div className="search">
-        <input></input>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="search"></input>
+        </form>
       </div>
     </div>
   );
